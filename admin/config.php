b2ca13b64c793279cc3feef479372c3bc0fbc5d2
luisse <oppeluis@gmail.com>
2017-12-23 13:01:22 -0300

    Repositorio base de benjabikes

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:9099/admin/');
+define('HTTP_CATALOG', 'http://localhost:9099/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:9099/admin/');
+define('HTTPS_CATALOG', 'http://localhost:9099/');
+
+// DIR
+define('DIR_APPLICATION', '/home/luis/PHP/benjabike_oc/admin/');
+define('DIR_SYSTEM', '/home/luis/PHP/benjabike_oc/system/');
+define('DIR_IMAGE', '/home/luis/PHP/benjabike_oc/image/');
+define('DIR_STORAGE', '/home/luis/PHP/storage/');
+define('DIR_CATALOG', '/home/luis/PHP/benjabike_oc/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '4Lso07');
+define('DB_DATABASE', 'benjabike_oc');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:9099/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:9099/');
+
+// DIR
+define('DIR_APPLICATION', '/home/luis/PHP/benjabike_oc/catalog/');
+define('DIR_SYSTEM', '/home/luis/PHP/benjabike_oc/system/');
+define('DIR_IMAGE', '/home/luis/PHP/benjabike_oc/image/');
+define('DIR_STORAGE', '/home/luis/PHP/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '4Lso07');
+define('DB_DATABASE', 'benjabike_oc');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
